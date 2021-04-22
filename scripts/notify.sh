a80ac5eaaaba327ef249002f856726588e9ac9c8
Stavros Foteinopoulos <stafot@gmail.com>
2021-04-22 18:46:33 +0300

    Fix: Try to fix parse issue on release notify. (#9)
    Failed to parse correctly special characters.
    Remove debugging lines from release script.

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -9,5 +9,5 @@ REPO=$(echo $GITHUB_CONTEXT | jq -r '.repository')
 TAGVERSION=$(echo $GITHUB_CONTEXT | jq -r '.event.release.tag_name')
 TAGURL=$(echo $GITHUB_CONTEXT | jq -r '.event.release.html_url')
 BODY=$(echo $GITHUB_CONTEXT | jq -r '.event.release.body' | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')
-echo "{\"username\":\"Cloud Bot Notify\",\"icon_url\":\"https://www.mattermost.org/wp-content/uploads/2016/04/icon.png\",\"text\":\"# **New Release for $REPO** - Release [$TAGVERSION]($TAGURL)\n $BODY\"}" > mattermost.json
+echo "{\"username\":\"Cloud Bot Notify\",\"icon_url\":\"https://www.mattermost.org/wp-content/uploads/2016/04/icon.png\",\"text\":\"# **New Release for $REPO** - Release [$TAGVERSION]($TAGURL)\n '$BODY'\"}" > mattermost.json
 cat mattermost.json
--- scripts/release.sh
+++ scripts/release.sh
@@ -7,22 +7,19 @@
 set -e
 
 FUTURE_RELEASE_SHA=$(hub rev-parse HEAD)
-LATEST_RELEASE=$(hub release)
+LATEST_RELEASE=$(hub release | head -n 1)
 LATEST_RELEASE_SHA=$(hub rev-parse ${LATEST_RELEASE})
 LATEST_RELEASE_NEXT_COMMIT_SHA=$(hub log ${LATEST_RELEASE}..HEAD --oneline --pretty=%H | tail -n1)
 mkdir -p ./build/_output/docs/
 release-notes --org mattermost --repo rotatorctl --start-sha $LATEST_RELEASE_NEXT_COMMIT_SHA --end-sha $FUTURE_RELEASE_SHA  --output ./build/_output/docs/relnote.md --required-author "" --branch main
 
-cat ./build/_output/docs/relnote.md | sed '/docs.k8s.io/ d' | sed -e "s/Release notes for/Release notes for ${CIRCLE_TAG}/g" | sed -e "s/Changelog since/Changelog since ${LATEST_RELEASE}/g"> ./build/_output/docs/relnote_parsed.md
+cat ./build/_output/docs/relnote.md | sed '/docs.k8s.io/ d' | sed -e "s/Release notes for/${CIRCLE_TAG}/g" | sed -e "s/Changelog since/Changelog since ${LATEST_RELEASE}/g"> ./build/_output/docs/relnote_parsed.md
 echo "\n" >> ./build/_output/docs/relnote_parsed.md
 echo "_Thanks to all our contributors!_" >> ./build/_output/docs/relnote_parsed.md
 mv ./build/_output/docs/relnote_parsed.md ./build/_output/docs/relnote.md
 
 make build && make build-mac
 mv  ./build/_output/bin/rotatorctl ./build/_output/bin/rotatorctl-linux
-echo "Debugging lines."
-pwd
-ls -lah ./build/_output/docs/
-ls -lah ./build/_output/bin/
+
 hub release create -d -F ./build/_output/docs/relnote.md -a ./build/_output/bin/rotatorctl-linux -a ./build/_output/bin/rotatorctl-darwin ${CIRCLE_TAG}
 rm -rf ./build/_output/
