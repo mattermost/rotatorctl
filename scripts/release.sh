f27d44682a8689d2b8dce1b5781b767f4f73730c
Stavros Foteinopoulos <stafot@gmail.com>
2021-04-23 15:22:58 +0300

    Show trace and use echo -e instead. (#11)

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,7 +4,7 @@
 # Note: To run this script locally you need to export environment variables CIRCLE_TAG and GITHUB_TOKEN.
 
 # Stop script on first error
-set -e
+set -xe
 
 FUTURE_RELEASE_SHA=$(hub rev-parse HEAD)
 LATEST_RELEASE=$(hub release | head -n 1)
@@ -13,8 +13,8 @@ LATEST_RELEASE_NEXT_COMMIT_SHA=$(hub log "${LATEST_RELEASE}"..HEAD --oneline --p
 mkdir -p ./build/_output/docs/
 release-notes --org mattermost --repo rotatorctl --start-sha "${LATEST_RELEASE_NEXT_COMMIT_SHA}" --end-sha "${FUTURE_RELEASE_SHA}"  --output ./build/_output/docs/relnote.md --required-author "" --branch main
 cat ./build/_output/docs/relnote.md | sed '/docs.k8s.io/ d' | sed -e "s/\# Release notes for/${CIRCLE_TAG}/g" | sed -e "s/Changelog since/Changelog since ${LATEST_RELEASE}/g"> ./build/_output/docs/relnote_parsed.md
-printf "\n" >> ./build/_output/docs/relnote_parsed.md
-printf "_Thanks to all our contributors!_" >> ./build/_output/docs/relnote_parsed.md
+echo -e "\n" >> ./build/_output/docs/relnote_parsed.md
+echo -e "_Thanks to all our contributors!_ \n" >> ./build/_output/docs/relnote_parsed.md
 mv ./build/_output/docs/relnote_parsed.md ./build/_output/docs/relnote.md
 
 make build && make build-mac
